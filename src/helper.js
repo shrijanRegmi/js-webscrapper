c426c8351d17898712e7e59c9d007731daa1392b
Shrijan Regmi <your.email@example.com>
2023-12-17 20:47:20 +0545

    feat: removed comments; replaced all the text contents with random text

--- src/helper.js
+++ src/helper.js
@@ -21,19 +21,41 @@ const convertUrlToData = async (url) => {
       const styleTags = documentBody.querySelectorAll("style");
       styleTags.forEach((styleTag) => styleTag.remove());
 
+      // remove all ins tags from body
+      const insTags = documentBody.querySelectorAll("ins");
+      insTags.forEach((insTag) => insTag.remove());
+
+      // remove all id attributes from body
+      const idAttributes = documentBody.querySelectorAll("[id]");
+      idAttributes.forEach((idAttribute) => idAttribute.setAttribute("id", ""));
+
+      // remove all class attributes from body
+      const classAttributes = documentBody.querySelectorAll("[class]");
+      classAttributes.forEach((classAttribute) => classAttribute.setAttribute("class", ""));
+
       // remove all src attributes from body
       const srcAttributes = documentBody.querySelectorAll("[src]");
-      srcAttributes.forEach((srcAttribute) => srcAttribute.removeAttribute("src"));
+      srcAttributes.forEach((srcAttribute) => srcAttribute.setAttribute("src", ""));
 
       // remove all href attributes from body
       const hrefAttributes = documentBody.querySelectorAll("[href]");
-      hrefAttributes.forEach((hrefAttribute) => hrefAttribute.removeAttribute("href"));
+      hrefAttributes.forEach((hrefAttribute) => hrefAttribute.setAttribute("href", ""));
 
       // remove all style attributes from body
       const styleAttributes = documentBody.querySelectorAll("[style]");
       styleAttributes.forEach((styleAttribute) => styleAttribute.removeAttribute("style"));
 
-      return JSON.stringify({[document.URL]: documentBody.innerHTML.replace(/\n/g, '')});
+      // remove all the \n and \t from the HTML
+      let innerHTML = documentBody.innerHTML;
+      innerHTML = innerHTML.replace(/\n/g, '').replace(/\t/g, '');
+
+      // remove all the comments from the HTML
+      innerHTML = innerHTML.replace(/<!--[\s\S]*?-->/g, '');
+
+      // replace all the text in the body with ACEMACEMACEMACEMACEMACEM
+      innerHTML = innerHTML.replace(/>(.*?)</g, '>ACEMACEMACEMACEMACEMACEM<');
+
+      return JSON.stringify({ [document.URL]: innerHTML });
     });
 
     return data;
@@ -46,5 +68,16 @@ const convertUrlToData = async (url) => {
 const getFinalData = (variable, finalData) => {
   return `const ${variable} = [${finalData.toString()}];\n\nexport default ${variable};`;
 };
+
+// Traverse through all text nodes and replace their content with an empty string
+function removeAllTextNodes(element) {
+  if (element.nodeType === 3) { // Text node
+      element.nodeValue = '';
+  } else {
+      for (var i = 0; i < element.childNodes.length; i++) {
+          removeAllTextNodes(element.childNodes[i]);
+      }
+  }
+}
 
 export { convertUrlToData, getFinalData };
